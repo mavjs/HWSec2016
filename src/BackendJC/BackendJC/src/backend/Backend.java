f5669f07089c50fcf732bba48ee5d6f4d7bdf9da
Stephanie Silvius <s.silvius@student.science.ru.nl>
2016-05-15 13:18:50 +0200

    Fixed certificate creation issues, and worked on CRL (almost done)

---
+++ src/BackendJC/BackendJC/src/backend/Backend.java
@@ -0,0 +1,228 @@
+package backend;
+
+import java.io.*;
+import java.security.*;
+import java.security.cert.CertificateException;
+import java.security.interfaces.*;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+//import java.util.LinkedList;
+import java.math.BigInteger;
+import java.security.spec.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import org.jmrtd.cert.*;
+import org.bouncycastle.*;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.ejbca.cvc.AccessRightEnum;
+import org.ejbca.cvc.AccessRightSignTermEnum;
+import org.ejbca.cvc.AccessRights;
+import org.ejbca.cvc.AuthorizationRole;
+import org.ejbca.cvc.AuthorizationRoleEnum;
+import org.ejbca.cvc.CAReferenceField;
+import org.ejbca.cvc.CVCertificate;
+import org.ejbca.cvc.CVCertificateBody;
+import org.ejbca.cvc.CertificateGenerator;
+//import org.ejbca.cvc.*;
+//import org.ejbca.cvc.CVCAuthorizationTemplate;
+//import org.ejbca.cvc.CardVerifiableCertificate;
+import org.ejbca.cvc.HolderReferenceField;
+
+public class Backend implements PublicKey, PrivateKey, GlobVarBE{
+	CertificateCreator certificateCreator; 
+	BouncyCastleProvider provider;
+	final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+	//TODO: List of revoked certificates
+	//TODO: List of IDs associated with each terminal and card  --> Request from personalisation terminal? 
+	//TODO: List of revoked petrol cards
+	
+	public Backend() throws Exception {
+		certificateCreator = new CertificateCreator();
+		KeyPair keypair_backend = RSAKeyGen();
+		KeyPair keypair_terminal = RSAKeyGen();
+		
+		CVCertificate cert_backend = certificateCreator.createCertificate(CertType.CA, keypair_backend, keypair_terminal);
+		CVCertificate cert_terminal = certificateCreator.createCertificate(CertType.TERMINAL, keypair_backend, keypair_terminal);
+		addToCRL("T", cert_terminal , new Date(), 4);
+		addToCRL("C", cert_terminal , new Date(), 20);
+
+		isOnCRL(keypair_terminal,provider,cert_terminal,cert_backend);
+		String certCA =  cert_backend.getAsText();
+//		System.out.print(certCA);
+//		String certTerm = cert_terminal.getAsText();
+//		System.out.print(certTerm);
+	}	
+	
+	
+	/* For every transaction this method is called. 
+	 * Verification will be done if the card and terminal are on the CRL
+	 * 		IF YES: transaction should be stopped and entry should be logged?
+	 * 		IF NOT: continue transaction
+	 */
+	
+
+	public static boolean isCertificateValid(String tag, int serialNr, int allowance, CardVerifiableCertificate cert){
+		try {
+			Date dateRevoke;
+			byte[] signature = cert.getSignature();
+
+	//		cert.getAuthorityReference();
+			return false;
+		} catch (Exception e){
+			System.out.println("Error in validity checking:" + e.getMessage());
+			return false;
+		}
+		
+		// verify if card is on CRL list
+		//verify if Cert is signed by private key.
+		// verify if date of certificate is still valid
+	}
+	/* Purpose is to convert signature to a full length string, otherwise you get garbage. */
+	public static String bytesToHex(byte[] bytes) {
+	    char[] hexChars = new char[bytes.length * 2];
+	    for ( int j = 0; j < bytes.length; j++ ) {
+	        int v = bytes[j] & 0xFF;
+	        hexChars[j * 2] = hexArray[v >>> 4];
+	        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+	    }
+	    return new String(hexChars);
+	}
+	
+	/* CRL consists of: 
+	Tag (C or T), C = Card T = Terminal
+	id = ID number of card or terminal
+	date-time of revocation, so current date
+	allowance before revocation 
+	Certificate hash
+	
+	*/
+	
+	// tagKey is 
+	public static boolean isOnCRL(KeyPair tagKey, BouncyCastleProvider provider, CVCertificate cert, CVCertificate cert_ca) throws NoSuchFieldException{
+	//	StringBuilder sb = new StringBuilder();
+		String holderRef = cert.getCertificateBody().getHolderReference().getConcatenated();
+		Date validFrom = cert.getCertificateBody().getValidFrom();
+		Date validTo = cert.getCertificateBody().getValidTo();
+		
+		byte[] signature = cert.getSignature();
+		String hash = bytesToHex(signature);
+		
+		try {
+			String line;
+			BufferedReader buffer = new BufferedReader(new FileReader(CRL_FILE));
+			Boolean found = false;
+				
+			 while((line = buffer.readLine()) != null) {
+				 System.out.println("line starts here:");
+				 System.out.println( line);
+				 if(line.indexOf('20230E5C1F76CE19') != -1) {
+					 found = true;			 
+					 break;
+				 }
+			 }
+			return found;
+		} catch (Exception e){
+			System.out.println("Error for CRL:" + e.getClass() + e.getMessage());
+			return false;
+		}
+/*		} finally {
+			try {
+				return false;
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				System.out.println("Error in adding to CRL: " + e.getMessage());
+				return false;
+			}		
+		} */
+		
+	}
+	
+	public static void addToCRL(String tag, CVCertificate cert, Date dateRevoke, int allowance ) throws NoSuchFieldException{
+		FileWriter fileWriter = null;
+		byte[] signature = cert.getSignature();
+		String hash = bytesToHex(signature);
+		
+		try {
+			fileWriter = new FileWriter(CRL_FILE,true);
+			
+			fileWriter.append(FILE_HEADER.toString());
+			fileWriter.append(NEW_LINE_SEPERATOR.toString());			
+			fileWriter.append(String.valueOf(tag));
+			fileWriter.append(COMMA_DELIMITER);
+			fileWriter.append(String.valueOf(hash));
+			fileWriter.append(COMMA_DELIMITER);
+			fileWriter.append(String.valueOf(dateRevoke));
+			fileWriter.append(COMMA_DELIMITER);
+			fileWriter.append(String.valueOf(allowance));
+			fileWriter.append(NEW_LINE_SEPERATOR);		
+			System.out.println("Sweet, CSV succesful");		
+		} catch (Exception e){
+			System.out.println("Error in adding to CRL:" + e.getMessage());
+		} finally {
+			try {
+				fileWriter.flush();
+				fileWriter.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				System.out.println("Error in adding to CRL: " + e.getMessage());
+			}		
+		}
+	}
+	
+	
+	public void removeFromCRL(){
+		
+	}
+
+	@Override
+	public String getAlgorithm() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+	@Override
+	public String getFormat() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	// 1024 bits is not sufficient, but enough for testing phase
+	public KeyPair RSAKeyGen() {
+		try {
+			System.out.println("Generating RSA keys, please wait...");
+			final KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+			keyGen.initialize(RSA_BITS, new SecureRandom());
+			final KeyPair keyPair = keyGen.generateKeyPair();		
+			return keyPair;				
+		} catch (Exception e) {
+			System.out.println("Error in RSA key generation:" + e.getMessage());
+			return null;
+		}	
+	}
+	
+	public static void writeKey(Key key, String filename) throws IOException {
+		FileOutputStream file = new FileOutputStream(filename);
+	//	file.write(key);
+		file.close();
+	}
+	
+	// Setting the monthly allowance that will be distributed to all chargingterminals
+	public short monthlyAllowance(){
+		//Maybe perform a check if a terminal is a valid CT. 
+		short allowance = 50; 	//Max. value is 32767 (inclusive).
+		return allowance;
+	}
+
+	@Override
+	public byte[] getEncoded() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ src/BackendJC/BackendJC/src/backend/CertificateCreator.java
@@ -0,0 +1,135 @@
+package backend;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Security;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Random;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.ejbca.cvc.AccessRightEnum;
+import org.ejbca.cvc.AccessRights;
+import org.ejbca.cvc.AuthorizationRoleEnum;
+import org.ejbca.cvc.CAReferenceField;
+import org.ejbca.cvc.CVCertificate;
+import org.ejbca.cvc.CertificateGenerator;
+import org.ejbca.cvc.HolderReferenceField;
+
+enum CertType{
+	CA, TERMINAL, CARD;
+}
+
+public class CertificateCreator implements GlobVarBE{
+	final protected static char[] serialArray = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+
+	public CertificateCreator()throws Exception{
+		
+	}
+	// TODO: no verification if a number has already been generated. Maybe if there is still time..
+	protected String createRandomString(int length)
+	{
+		char[] serialChars = new char[length];
+	    for(int i = 0; i<length; i++){
+	        serialChars[i] = serialArray[new Random().nextInt(36)];
+	    }
+	    return new String(serialChars);
+	}
+			
+	public CVCertificate createCertificate(CertType certType, KeyPair keypairCA, KeyPair keypairTerm) throws Exception {
+		CAReferenceField caRef = new CAReferenceField("NL", "DUMMY", "00000");
+		HolderReferenceField holderRef = new HolderReferenceField("NL", "DUMMY", "00000");
+		String serialNrCA = "";
+		String serialNrTerm = "";
+		String serialNrCard = "";
+		String ID = "";
+		String type = "";
+		
+		// TODO: figure out different roles and rights DV_F = Foreign, DV_D = Domestic, IS Extended Inspection System
+		// See: www.commoncriteria.org/files/ppfiles/pp0056_V2b_pdf.pdf
+		// DG3 = Fingerprint
+		// DG4 = Irisscan
+		AccessRights rights;
+		AuthorizationRoleEnum role;
+		PublicKey publicKey;
+		PrivateKey privateKey;
+		
+		switch(certType){
+		case CA:
+			serialNrCA = createRandomString(5);
+			caRef = new CAReferenceField("NL", "PetrolCA", serialNrCA);
+			holderRef = new HolderReferenceField("NL", "PetrolCA", serialNrCA);
+			rights = AccessRightEnum.READ_ACCESS_DG3_AND_DG4;
+			role = AuthorizationRoleEnum.CVCA;
+			type = "Type: CA Certificate";
+			// private key CA for signing certificate
+			privateKey = keypairCA.getPrivate();
+			// public key Terminal for in certificate
+			publicKey = keypairCA.getPublic();
+			//return caRef, holderRef;
+			break;
+		case TERMINAL:
+			serialNrTerm = createRandomString(5);
+			caRef = new CAReferenceField("NL", "PetrolCA", serialNrTerm);
+			holderRef = new HolderReferenceField("NL", "PCATerm", serialNrTerm);  //PCA = Petrol CA
+			rights = AccessRightEnum.READ_ACCESS_DG3;
+			role = AuthorizationRoleEnum.DV_D;
+			type = "Type: Terminal Certificate";
+			privateKey = keypairCA.getPrivate();
+			// public key Terminal for in certificate
+			publicKey = keypairTerm.getPublic();
+			ID = createRandomString(9);
+			break;
+		case CARD: 
+		default: 		// In case the role or rights have not been specified, this will be default setting. 
+			serialNrCard = createRandomString(5);
+			caRef = new CAReferenceField("NL", "PetrolCA", serialNrCard);
+			holderRef = new HolderReferenceField("NL", "PCACard", serialNrCard);
+			rights = AccessRightEnum.READ_ACCESS_NONE;
+			role = AuthorizationRoleEnum.DV_F;
+			type = "Type: Card Certificate";
+			privateKey = keypairCA.getPrivate();
+			// public key Terminal for in certificate
+			publicKey = keypairTerm.getPublic();
+			ID = createRandomString(9);
+			break;
+		}
+		
+		FileWriter fileWriter = null;
+		Calendar cal = Calendar.getInstance();
+
+		String algorithmName = "SHA1WITHRSA";
+		BouncyCastleProvider provider = new BouncyCastleProvider();
+		Security.addProvider(provider);
+
+		Date validFrom = new Date();
+		cal.set(Calendar.YEAR, cal.get(Calendar.YEAR)+1); //Certificates will blow up on Feb 29th. :)
+		Date validTo = cal.getTime();
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	
+		try {		
+			CVCertificate cert = CertificateGenerator.createCertificate(
+					publicKey, privateKey, algorithmName, caRef, holderRef, role, rights,
+					validFrom, validTo, provider.getName());
+			String body = cert.getAsText();
+			fileWriter = new FileWriter(CA_CERT, true);
+			fileWriter.append("-----BEGIN-CERTIFICATE----------------\n");
+			fileWriter.append(type);
+			fileWriter.append(NEW_LINE_SEPERATOR.toString());
+			fileWriter.append(String.valueOf(body));
+			fileWriter.append("\n-----END-CERTIFICATE------------------\n");
+			return cert;
+		} catch (Exception e){
+			System.out.println("Error in certificate creation: " + e.getMessage());
+			return null;
+		} finally {
+			fileWriter.flush();
+			fileWriter.close();
+		}
+	}
+}
